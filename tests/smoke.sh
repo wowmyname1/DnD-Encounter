f893daa4c09705415a07b439b0fc8d4ac8c1fa8f
wowmyname1 <126262092+wowmyname1@users.noreply.github.com>
2026-08-18 07:01:07 +0000

    refactor: split extensions into data and logic

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -4,19 +4,24 @@ test -f index.html
 test -f styles.css
 test -f utils.js
 test -f app-core.js
-test -f app-extensions.js
+test -f extensions-data.js
+test -f extensions-logic.js
 
-test -s app-core.js
-test -s app-extensions.js
+test -s extensions-data.js
+test -s extensions-logic.js
 
 grep -Fq '<link rel="stylesheet" href="styles.css">' index.html
 grep -Fq '<script src="utils.js"></script>' index.html
 grep -Fq '<script src="app-core.js"></script>' index.html
-grep -Fq '<script src="app-extensions.js"></script>' index.html
+grep -Fq '<script src="extensions-data.js"></script>' index.html
+grep -Fq '<script src="extensions-logic.js"></script>' index.html
 
 grep -Fq 'window.AppEvents' utils.js
 grep -Fq 'AppEvents.emit("turn:end"' app-core.js
 grep -Fq 'escapeHtml(c.name)' app-core.js
-grep -Fq 'executeStatusTriggers' app-extensions.js
+
+grep -Fq 'SPELL_CATALOG' extensions-data.js
+grep -Fq 'executeStatusTriggers' extensions-logic.js
+grep -Fq 'initExtensions' extensions-logic.js
 
 echo smoke OK
